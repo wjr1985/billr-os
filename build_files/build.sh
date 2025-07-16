690ce5050534adabdfb75a2cab217300f962786e
Bill Rastello <wjr1985@gmail.com>
2025-07-16 10:17:30 -0500

    Update build script to install some preferred packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,35 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Define packages to install
+# To add a new package, simply add it to this array
+PACKAGES=(
+    autoconf
+    bzip2
+    bzip2-devel
+    gcc
+    gdbm-devel
+    libffi-devel
+    libuuid-devel
+    libyaml-devel
+    make
+    ncurses-devel
+    neovim
+    openssl-devel
+    patch
+    perl-FindBin
+    plasma-workspace-x11
+    python3-neovim
+    readline-devel
+    rust
+    sqlite-devel
+    tk-devel
+    tmux
+    zlib-ng-compat-devel
+)
+
+# Install all packages from fedora repos
+dnf5 install -y "${PACKAGES[@]}"
 
 # Use a COPR Example:
 #
@@ -21,4 +48,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
